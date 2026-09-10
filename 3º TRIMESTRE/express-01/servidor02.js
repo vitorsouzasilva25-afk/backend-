f1738ad2ceb9c9adbb6b8f7e9069fbac632d1b88
vitorsouzasilva25-afk <vitor.souza.silva25@escola.pr.gov.br>
2026-09-10 13:23:32 +0000

    Criação das páginas index, produtos e serviços, além da implementação do servidor com rotas

--- 3º TRIMESTRE/express-01/servidor02.js
+++ 3º TRIMESTRE/express-01/servidor02.js
@@ -1,11 +1,12 @@
 import express from  'express'
- 
+import path from 'path'
+
 const app = express()
 const porta = 3000
 
 // criar rotas de servidor 
-app.get('/',  (req, res) => {
-    res.send('olá, abacaxi!')
+app.get('/', (req, res) => {
+    res.sendFile('/paginas/index.html', {root: import.meta.dirname})
 })
 
 //liberar a porta do meu computador
